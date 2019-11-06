4c18d84e4da578fc2a6da6e0dd5a1aa78959db55
kvraamkey <ramki.ayyamperumal@everestdx.com>
2019-11-07 01:17:24 +0530

    removed unused files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+
+// Import all the third party stuff
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {render} from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Import root app
+import App from './App';
+
+const moundNode = document.getElementById("edxi");
+
+function renderApp() {
+    render(<App/>, moundNode)
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
