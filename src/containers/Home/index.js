b793ac4d31bb350fc4c1e5ba7eca310af54b0aa3
kvraamkey <ramki.ayyamperumal@everestdx.com>
2019-12-12 16:44:45 +0530

    sample page design

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function HomePage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="App">
             <h1>Welcome to EverestDx</h1>
+            <a class="App-link" href="/" target="_blank" rel="noopener noreferrer">Introduction</a>
         </div>
     )
 }
